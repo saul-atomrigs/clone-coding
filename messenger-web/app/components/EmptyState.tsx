33e14453349adea818cebab98ee214cb0e1cb1a6
Saul Lee <solleedata@gmail.com>
2023-07-26 11:37:28 +0900

    feat: EmptyState 컴포넌ㅌ트

---
+++ messenger-web/app/components/EmptyState.tsx
@@ -0,0 +1,9 @@
+export default function EmptyState() {
+  return (
+    <div className='px-4 py-10 sm:px-6 lg:px-8 lg:py-6 h-full flex justify-center items-center bg-gray-100'>
+      <div className='text-center items-center flex flex-col'>
+        <h3 className='mt-2 text-2xl font-semibold text-gray-900'>채팅방에 입장하거나 새로운 채팅방을 만들어 보세요.</h3>
+      </div>
+    </div>
+  );
+}
