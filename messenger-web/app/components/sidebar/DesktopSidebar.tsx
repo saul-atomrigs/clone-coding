78257dd3e4e1ef65e02a16b4a14437ea27394710
Saul Lee <saul@atomrigs.io>
2023-08-07 12:04:29 +0900

    feat: Sidebar, DesktopSidebar, DesktopItem

---
+++ messenger-web/app/components/sidebar/DesktopSidebar.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { useState } from 'react';
+import useRoutes from '@/app/hooks/useRoutes';
+import SettingsModal from './SettingsModal';
+import Avatar from '../Avatar';
+import DesktopItem from './DesktopItem';
+import { User } from '@prisma/client';
+
+type DesktopSidebarProps = {
+  currentUser: User;
+};
+
+export default function DesktopSidebar({ currentUser }: DesktopSidebarProps) {
+  const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <SettingsModal currentUser={currentUser} isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      <div
+        className='
+              hidden 
+              lg:fixed 
+              lg:inset-y-0 
+              lg:left-0 
+              lg:z-40 
+              lg:w-20 
+              xl:px-6
+              lg:overflow-y-auto 
+              lg:bg-white 
+              lg:border-r-[1px]
+              lg:pb-4
+              lg:flex
+              lg:flex-col
+              justify-between
+      '
+      >
+        <nav className='mt-4 flex flex-col justify-between'>
+          <ul className='flex flex-col items-center space-y-1' role='list'>
+            {routes.map((route) => {
+              <DesktopItem
+                key={route.label}
+                href={route.href}
+                label={route.label}
+                icon={route.icon}
+                active={route.active}
+                onClick={route.onClick}
+              />;
+            })}
+          </ul>
+        </nav>
+        <nav className='mt-4 flex flex-col justify-between items-center'>
+          <div onClick={() => setIsOpen(true)} className='cursor-pointer hover:opacity-75 transition'>
+            <Avatar user={currentUser} />
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
