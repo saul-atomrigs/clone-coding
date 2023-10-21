cf6db817804e685e3df886d2aa3894c7f8dc5141
Saul Lee <solleedata@gmail.com>
2023-10-21 17:32:08 +0900

    chore: hooks doc 추가

--- messenger-web/app/hooks/useOtherUser.ts
+++ messenger-web/app/hooks/useOtherUser.ts
@@ -3,12 +3,21 @@ import { User } from '@prisma/client';
 import { useSession } from 'next-auth/react';
 import { useMemo } from 'react';
 
+/**
+ * This function returns the other user in a conversation, given the conversation object and the current session.
+ * If the conversation object is of type FullConversationType, it will directly extract the users array.
+ * If the conversation object does not have the users array, it will assume that the conversation object itself is the users array.
+ * 
+ * @param conversation - The conversation object or the object containing the users array.
+ * @returns The other user in the conversation.
+ */
 export default function useOtherUser(conversation: FullConversationType | { users: User[] }) {
   const session = useSession();
 
   const otherUser = useMemo(() => {
     const currentUserEmail = session?.data?.user?.email;
 
+    // Filter out the current user from the users array
     const otherUser = conversation.users.filter((user) => user.email !== currentUserEmail);
 
     return otherUser[0];
--- messenger-web/app/hooks/useRoutes.ts
+++ messenger-web/app/hooks/useRoutes.ts
@@ -5,34 +5,38 @@ import { HiChat } from 'react-icons/hi';
 import { HiUsers, HiArrowLeftOnRectangle } from 'react-icons/hi2';
 import { signOut } from 'next-auth/react';
 
-/** Route들을 정의함 */
+/**
+ * Returns an array of routes used in the application.
+*/
 export default function useRoutes() {
   const pathname = usePathname();
   const { conversationId } = useConversation();
+  
+  /**
+   * Each route object contains a label, href, icon, and active property.
+   * The active property of each route is determined based on the current pathname or conversationId.
+   */
+  const routes = useMemo(() => [
+    {
+      label: 'Chat',
+      href: '/conversations',
+      icon: HiChat,
+      active: pathname === '/conversations' || !!conversationId,
+    },
+    {
+      label: 'Users',
+      href: '/users',
+      icon: HiUsers,
+      active: pathname === '/users',
+    },
+    {
+      label: 'Logout',
+      onClick: () => signOut(),
+      href: '#',
+      icon: HiArrowLeftOnRectangle,
+    },
+  ], [pathname, conversationId]);
 
-  const routes = useMemo(
-    () => [
-      {
-        label: 'Chat',
-        href: '/conversations',
-        icon: HiChat,
-        active: pathname === '/conversations' || !!conversationId,
-      },
-      {
-        label: 'Users',
-        href: '/users',
-        icon: HiUsers,
-        active: pathname === '/users',
-      },
-      {
-        label: 'Logout',
-        onClick: () => signOut(),
-        href: '#',
-        icon: HiArrowLeftOnRectangle,
-      },
-    ],
-    [pathname, conversationId]
-  );
-
+  // Return the routes array
   return routes;
 }
