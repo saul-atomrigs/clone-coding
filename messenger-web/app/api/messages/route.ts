28343e9ef782d9b3678aaa2ee40d933ce6ed0b82
Saul Lee <solleedata@gmail.com>
2023-10-31 11:24:36 +0900

    fix: Remove 'default export' in api/messages/route

--- messenger-web/app/api/messages/route.ts
+++ messenger-web/app/api/messages/route.ts
@@ -7,22 +7,17 @@ import { pusherServer } from '@/app/libs/pusher';
  * Creates a new message and updates the conversation.
  * 새로운 메시지를 만들고 채팅 목록을 업데이트합니다
  */
-export default async function POST(request: Request) {
-  try {
-    // Get the current user
-    const currentUser = await getCurrentUser();
 
-    // Parse the request body
+export async function POST(request: Request) {
+  try {
+    const currentUser = await getCurrentUser();
     const body = await request.json();
     const { message, image, conversationId } = body;
 
-    // Check if the user is authenticated
     if (!currentUser?.id || !currentUser?.email) {
-      // Return an unauthorized response
       return new NextResponse('Unauthorized', { status: 401 });
     }
 
-    // Create a new message
     const newMessage = await prisma.message.create({
       include: {
         seen: true,
@@ -45,7 +40,6 @@ export default async function POST(request: Request) {
       },
     });
 
-    // Update the conversation
     const updatedConversation = await prisma.conversation.update({
       where: {
         id: conversationId,
@@ -68,14 +62,11 @@ export default async function POST(request: Request) {
       },
     });
 
-    // Trigger a pusher event for the new message
-    await pusherServer.trigger(conversationId, 'messages: new', newMessage);
+    await pusherServer.trigger(conversationId, 'messages:new', newMessage);
 
-    // Get the last message in the updated conversation
     const lastMessage =
       updatedConversation.messages[updatedConversation.messages.length - 1];
 
-    // Trigger a pusher event to update each user's conversation
     updatedConversation.users.map((user) => {
       pusherServer.trigger(user.email!, 'conversation:update', {
         id: conversationId,
@@ -83,10 +74,8 @@ export default async function POST(request: Request) {
       });
     });
 
-    // Return the new message as a JSON response
     return NextResponse.json(newMessage);
-  } catch (error: any) {
-    // Return a server error response
-    return new NextResponse('Something went wrong', { status: 500 });
+  } catch (error) {
+    return new NextResponse('Error', { status: 500 });
   }
 }
