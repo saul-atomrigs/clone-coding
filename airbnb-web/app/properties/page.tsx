95031a86517730278ee75163b8e6a75f90ffbbeb
saul <solleedata@gmail.com>
2024-01-08 00:35:13 +0900

    feat: properties UI

---
+++ airbnb-web/app/properties/page.tsx
@@ -0,0 +1,41 @@
+import EmptyState from "@/app/components/EmptyState"
+import ClientOnly from "@/app/components/ClientOnly"
+
+import getCurrentUser from "@/app/actions/getCurrentUser"
+import getListings from "@/app/actions/getListings"
+
+import PropertiesClient from "./PropertiesClient"
+
+/**
+ * Renders the properties page based on the user's authentication status and property listings.
+ *
+ * 유저 소유 properties 페이지
+ */
+const PropertiesPage = async () => {
+ const currentUser = await getCurrentUser()
+
+ if (!currentUser) {
+  return <EmptyState title="Unauthorized" subtitle="Please login" />
+ }
+
+ const listings = await getListings({ userId: currentUser.id })
+
+ if (listings.length === 0) {
+  return (
+   <ClientOnly>
+    <EmptyState
+     title="No properties found"
+     subtitle="Looks like you have no properties."
+    />
+   </ClientOnly>
+  )
+ }
+
+ return (
+  <ClientOnly>
+   <PropertiesClient listings={listings} currentUser={currentUser} />
+  </ClientOnly>
+ )
+}
+
+export default PropertiesPage
